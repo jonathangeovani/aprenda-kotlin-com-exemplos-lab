34b16ddea2774e75a8b61fb44bb33e6c83e4d420
Jonathan Geovani <dongeovanimenor@gmail.com>
2023-12-18 09:09:52 -0300

    feat: implement solution

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Aluno(val nome: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg novoAluno: Aluno) {
+        for (aluno in novoAluno) {
+            this.inscritos.add(aluno)
+        }
+        println("Alunos matriculados com sucesso!")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val joao = Aluno("João")
+    val maria = Aluno("Maria")
+    
+    val pooComKotlin = ConteudoEducacional("POO com Kotlin", 120)
+    val apiComKotlinESpringBoot = ConteudoEducacional("Criando uma API com Kotlin e Spring Boot", 180)
+    
+    val formacaoBackEnd = Formacao("Formação Dev Back end", listOf(pooComKotlin, apiComKotlinESpringBoot), Nivel.BASICO)
+    
+    formacaoBackEnd.matricular(joao, maria)
+    
+    println("\nAlunos matriculados no curso ${formacaoBackEnd.nome}:")
+    for (aluno in formacaoBackEnd.inscritos) {
+        println("Nome: " + aluno.nome)
+    }
+    
+    println("\nConteúdo do curso ${formacaoBackEnd.nome}:")
+    for (conteudo in formacaoBackEnd.conteudos) {
+        println("Nome: ${conteudo.nome} - Duração: ${conteudo.duracao}min")
+    }
+    
 }
